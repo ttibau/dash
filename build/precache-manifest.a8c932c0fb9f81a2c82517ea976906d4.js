fce54d93951a4fccb97d85881125b2f0abceda96
Catarina Ribeiro <catarinaribeiro@id.uff.br>
2019-03-08 00:41:58 -0300

    publish

---
+++ build/precache-manifest.a8c932c0fb9f81a2c82517ea976906d4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/dash/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/dash/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/dash/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/dash/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "4711a01bbd0588bdedc2",
+    "url": "/dash/static/js/runtime~main.4711a01b.js"
+  },
+  {
+    "revision": "23928bbc43d92682e32d",
+    "url": "/dash/static/js/main.23928bbc.chunk.js"
+  },
+  {
+    "revision": "120aae75bbdd361eec4d",
+    "url": "/dash/static/js/2.120aae75.chunk.js"
+  },
+  {
+    "revision": "23928bbc43d92682e32d",
+    "url": "/dash/static/css/main.2fc6408d.chunk.css"
+  },
+  {
+    "revision": "120aae75bbdd361eec4d",
+    "url": "/dash/static/css/2.dd23d2c8.chunk.css"
+  },
+  {
+    "revision": "9ed355ad690b4e2dfcd9f124dca7f6fd",
+    "url": "/dash/index.html"
+  }
+];
